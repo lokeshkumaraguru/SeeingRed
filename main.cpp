7cdb318b5da3f99af8a2c3b8d4507472040de44d
lokeshkumaraguru <lkumaraguru@mail.csuchico.edu>
2018-10-22 16:57:30 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ int main(int argc, char* argv[])
         color = imagePixels [row][column];
         color.green = 0;
         color.blue = 0;
-        color = imagePixels [row][column];
+        imagePixels [row][column] = color;
 
         }
       }
